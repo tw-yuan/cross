f15899bdc441bb36702c569bf5753936ab643bad
Yuan <yuan.ncse.tw@gmail.com>
2023-09-18 19:09:07 +0800

    update

--- index.js
+++ index.js
@@ -1,4 +1,27 @@
-const Discord = require("discord.js");
-const client = new Discord.Client(); 
+// initialize
+const { token } = require("./config.json");
+const { Client, GatewayIntentBits, Partials, Collection, ActivityType } = require('discord.js')
 
-client.login("Your Token");
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.GuildPresences,
+    GatewayIntentBits.GuildMessageReactions,
+    GatewayIntentBits.DirectMessages,
+    GatewayIntentBits.MessageContent
+  ],
+  partials: [Partials.Channel, Partials.Message, Partials.User, Partials.GuildMember, Partials.Reaction]
+});
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`)
+});
+
+client.on("message", msg => {
+  if (msg.content === "ping") {
+    msg.reply("pong");
+  }
+});
+
+client.login(token)
